502750f71e164ca46f941d2f2bd9c309c08fcebb
tahsinAmin <muhammadtahsinamin@gmail.com>
2020-12-05 17:24:49 +0600

    Getting Started

---
+++ details.php
@@ -0,0 +1,16 @@
+<?php
+	include('config/db_connect.php');
+
+	// check GET request id param
+	if (isset($_GET['id'])) {
+		$id = mysqli
+	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<?php include('templates/header.php'); ?>
+    details
+  <?php include('templates/footer.php'); ?>
+</html>
